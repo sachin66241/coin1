1213b593376466d006071891c8ad2ce207bd1734
blockchain-expert <blockchain@cybrosys.com>
2019-11-02 15:56:54 +0530

    Initial commit

---
+++ contract/contract.sol
@@ -0,0 +1,420 @@
+pragma solidity ^0.4.24;
+
+// ----------------------------------------------------------------------------
+
+// GMT Token contract
+//
+// Symbol      : GMT
+// Name        : GoldMint Token
+// Total supply: 1,000,000,000.000000000000000000
+// Decimals    : 18
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+
+// Safe maths
+
+// ----------------------------------------------------------------------------
+
+library SafeMath {
+
+    function add(uint a, uint b) internal pure returns (uint c) {
+
+        c = a + b;
+
+        require(c >= a);
+
+    }
+
+    function sub(uint a, uint b) internal pure returns (uint c) {
+
+        require(b <= a);
+
+        c = a - b;
+
+    }
+
+    function mul(uint a, uint b) internal pure returns (uint c) {
+
+        c = a * b;
+
+        require(a == 0 || c / a == b);
+
+    }
+
+    function div(uint a, uint b) internal pure returns (uint c) {
+
+        require(b > 0);
+
+        c = a / b;
+
+    }
+
+}
+
+
+// ----------------------------------------------------------------------------
+
+// ERC Token Standard #20 Interface
+
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+
+// ----------------------------------------------------------------------------
+
+contract ERC20Interface {
+
+    function totalSupply() public constant returns (uint);
+
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+
+    function transfer(address to, uint tokens) public returns (bool success);
+
+    function allowance(address holder, address spender) public constant returns (uint);
+
+    function transferFrom(address from, address to, uint value) public returns (bool ok);
+
+    function approve(address spender, uint value) public returns (bool ok);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+ 
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+
+
+// ----------------------------------------------------------------------------
+
+// Owned contract
+
+// ----------------------------------------------------------------------------
+
+contract Owned {
+
+    address public owner;
+
+    function transferOwnership(address newOwner) public returns(bool success);
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    event Freeze(bool success);
+
+    event SetRate(uint64 newRate);
+
+    constructor() public{
+
+        owner = msg.sender;
+
+    }
+
+
+    modifier onlyOwner {
+
+        require(msg.sender == owner);
+
+        _;
+
+    }
+}
+
+
+
+// ----------------------------------------------------------------------------
+
+// ERC20 Token, with the addition of symbol, name and decimals and an
+
+// initial fixed supply
+
+// ----------------------------------------------------------------------------
+
+contract GMT is ERC20Interface, Owned {
+
+    using SafeMath for uint;
+
+
+    string public symbol;
+
+    string public  name;
+
+    uint8 public decimals;
+
+    uint public _totalSupply;
+
+    uint64 public rate;
+
+    bool public freeze = false; //Freeze state
+
+
+    mapping(address => uint) balances;
+
+      /* approve() allowances */
+    mapping (address => mapping (address => uint)) allowed;
+
+    event Burn(address indexed from, uint256 value);
+
+    modifier isAvailable{
+        
+        require(!freeze);
+        
+        _;
+    }
+
+
+    // ------------------------------------------------------------------------
+
+    // Constructor
+
+    // ------------------------------------------------------------------------
+
+    constructor() public {
+
+        symbol = "GMT";
+
+        name = "GoldMint Token";
+
+        decimals = 18;
+
+        rate = 10;
+
+        _totalSupply = 1000000000 * 10**uint(decimals);
+
+        balances[owner] = _totalSupply;
+
+        emit Transfer(address(0), owner, _totalSupply);
+
+    }
+
+
+    // ------------------------------------------------------------------------
+
+    // Do accept ETH
+
+    // ------------------------------------------------------------------------
+
+
+
+    function () public payable {
+        require(
+            msg.value > 0 &&
+            isSoldout() == false &&
+            balances[owner] >= msg.value.mul(rate) &&
+            freeze==false
+        );
+      
+        uint256 tokens = msg.value.mul(rate);
+        balances[owner] = balances[owner].sub(tokens);
+        balances[msg.sender] = balances[msg.sender].add(tokens);
+        owner.transfer(msg.value);
+        emit Transfer (owner,msg.sender,tokens);
+
+    }
+
+
+     // ------------------------------------------------------------------------
+
+    // Change Ownership
+
+    // ------------------------------------------------------------------------
+
+
+    function transferOwnership(address newOwner) public onlyOwner isAvailable returns(bool success) {
+   
+        require(newOwner != address(0));
+        balances[newOwner] = balances[owner];
+        balances[owner] = 0;
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        return true;
+ 
+    }
+
+
+
+
+    // ------------------------------------------------------------------------
+
+    // Total supply
+
+    // ------------------------------------------------------------------------
+
+    function totalSupply() public view returns (uint) {
+
+        return _totalSupply  - balances[address(0)];
+
+    }
+    
+    // ------------------------------------------------------------------------
+
+    // Current Rate
+
+    // ------------------------------------------------------------------------
+
+    function currentRate() public view returns (uint) {
+
+        return rate;
+
+    }
+    
+    // ------------------------------------------------------------------------
+
+    // Current Owner
+
+    // ------------------------------------------------------------------------
+
+    function currentOwner() public view returns (address) {
+
+        return owner;
+
+    }
+
+    // ------------------------------------------------------------------------
+
+    // Current status
+
+    // ------------------------------------------------------------------------
+
+    function currentStatus() public view returns (bool) {
+
+        return freeze;
+
+    }
+
+
+
+
+    // ------------------------------------------------------------------------
+
+    // Get the token balance for account `tokenOwner`
+
+    // ------------------------------------------------------------------------
+
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+
+        return balances[tokenOwner];
+
+    }
+
+
+
+    // ------------------------------------------------------------------------
+
+    // Transfer the balance from token owner's account to `to` account
+
+    // - Owner's account must have sufficient balance to transfer
+
+    // - 0 value transfers are allowed
+
+    // ------------------------------------------------------------------------
+
+    function transfer(address to, uint tokens) public isAvailable returns (bool success) {
+
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+
+        balances[to] = balances[to].add(tokens);
+
+        emit Transfer(msg.sender, to, tokens);
+
+        return true;
+
+    }
+    
+    function setRate(uint64 newRate) public onlyOwner isAvailable returns (bool success) {
+        
+        rate = newRate;
+        emit SetRate(rate);        
+        return true;
+
+    }
+
+
+    function isSoldout() public view returns (bool) {
+
+        return(       
+            balances[owner] < 1
+        );
+    }
+
+
+    // ------------------------------------------------------------------------
+
+    // Returns the amount of tokens approved by the owner that can be
+
+    // transferred to the spender's account
+
+    // ------------------------------------------------------------------------
+
+    // function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+
+    //     return allowed[tokenOwner][spender];
+
+    // }
+
+
+    // ------------------------------------------------------------------------
+    // Owner can withdraw ether if token received.
+    // ------------------------------------------------------------------------
+    function withdraw() public onlyOwner returns (bool result) {
+        
+        return owner.send(address(this).balance);
+        
+    }
+    
+    // ------------------------------------------------------------------------
+
+    // Owner can transfer out any accidentally sent ERC20 tokens
+
+    // ------------------------------------------------------------------------
+
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner isAvailable returns (bool success) {
+
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+
+    }
+
+     /**
+     * Freeze and unfreeze ICO.
+     */
+    function freeze() public onlyOwner returns(bool success) {
+        freeze = true;
+        emit Freeze(freeze);
+        return freeze;
+    }
+
+    function unFreeze() public onlyOwner returns(bool success) {
+        freeze = false;
+        emit Freeze(freeze);
+        return freeze;
+    }
+
+    function burn() public isAvailable onlyOwner {
+        emit Burn(owner, balances[owner]);
+        balances[owner] = 0;
+    }
+
+    function approve(address _spender, uint _value) public isAvailable returns (bool success) {
+        // To change the approve amount you first have to reduce the addresses`
+        //  allowance to zero by calling `approve(_spender, 0)` if it is not
+        //  already 0 to mitigate the race condition described here:
+    
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _holder, address _spender) public constant returns (uint remaining) {
+        return allowed[_holder][_spender];
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public isAvailable returns (bool success) {
+        uint _allowance = allowed[_from][msg.sender];
+
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+}
---
+++ wallet/assets/js/contract.js
@@ -0,0 +1,687 @@
+setInterval(function(){
+	if (typeof web3 !== 'undefined') {
+		web3 = new Web3(web3.currentProvider);
+		if (web3.eth.coinbase == null) {
+			// console.log("////// locked ///////");
+			// console.log(web3.eth.coinbase)
+			// console.log("/////////////");
+			$("#modalHeading").text("Metamask is locked. Please unlock your Metamask.");
+			$("#modalText").text("Unlock metamask to access this portal");
+			$("#modalImage").attr("src","assets/img/MetamaskLocked.png");
+			$('.modal-overlay, .modal').removeClass('active');
+			$('.modal-overlay, .modal').addClass('active');
+			$('.close-modal').click(function () {
+				$('.modal-overlay, .modal').removeClass('active');
+			});
+		}
+		else if (web3.version.network != 3){
+			// console.log("//////wrong n/w///////");
+			// console.log(web3.version.network);
+			// console.log("/////////////");
+			$("#modalHeading").text("Metamask is in wrong network.");
+			$("#modalText").text("Please Change the network into 'Ropsten Network' .");
+			$("#modalImage").attr("src","assets/img/wrong-nw.png");
+			$('.modal-overlay, .modal').removeClass('active');
+			$('.modal-overlay, .modal').addClass('active');
+			$('.close-modal').click(function () {
+				$('.modal-overlay, .modal').removeClass('active');
+			});
+		}
+		
+		else{
+			$('.modal-overlay, .modal').removeClass('active');
+		}
+	
+	
+	}
+	
+	else {
+		var elements = $('.modal-overlay, .modal');
+	//	$("#modalText").text("Unlock metamask to access this portal");
+		$("#modalText").text("Install metamask to access this portal");
+		$("#modalHeading").text("Metamask not installed");
+		$("#modalImage").attr("src","assets/img/metamask.png");
+		$('.close-modal').click(function () {
+			elements.removeClass('active');
+		});
+		elements.addClass('active');
+	}	
+
+}, 1000);
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+	//////////////////////  code  ///////////////////////////////////////
+	const contractAddress = "0x64719730ea80fa20e297454e6c944b856c30f0fa";
+	const contractABI= [
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": true,
+					"name": "owner",
+					"type": "address"
+				},
+				{
+					"indexed": true,
+					"name": "spender",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "Approval",
+			"type": "event"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "_spender",
+					"type": "address"
+				},
+				{
+					"name": "_value",
+					"type": "uint256"
+				}
+			],
+			"name": "approve",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [],
+			"name": "burn",
+			"outputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": false,
+					"name": "newRate",
+					"type": "uint64"
+				}
+			],
+			"name": "SetRate",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": true,
+					"name": "previousOwner",
+					"type": "address"
+				},
+				{
+					"indexed": true,
+					"name": "newOwner",
+					"type": "address"
+				}
+			],
+			"name": "OwnershipTransferred",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": true,
+					"name": "from",
+					"type": "address"
+				},
+				{
+					"indexed": true,
+					"name": "to",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"name": "tokens",
+					"type": "uint256"
+				}
+			],
+			"name": "Transfer",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": true,
+					"name": "from",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "Burn",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": false,
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"name": "Freeze",
+			"type": "event"
+		},
+		{
+			"constant": false,
+			"inputs": [],
+			"name": "freeze",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "newRate",
+					"type": "uint64"
+				}
+			],
+			"name": "setRate",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "to",
+					"type": "address"
+				},
+				{
+					"name": "tokens",
+					"type": "uint256"
+				}
+			],
+			"name": "transfer",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "tokenAddress",
+					"type": "address"
+				},
+				{
+					"name": "tokens",
+					"type": "uint256"
+				}
+			],
+			"name": "transferAnyERC20Token",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "_from",
+					"type": "address"
+				},
+				{
+					"name": "_to",
+					"type": "address"
+				},
+				{
+					"name": "_value",
+					"type": "uint256"
+				}
+			],
+			"name": "transferFrom",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "newOwner",
+					"type": "address"
+				}
+			],
+			"name": "transferOwnership",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "constructor"
+		},
+		{
+			"payable": true,
+			"stateMutability": "payable",
+			"type": "fallback"
+		},
+		{
+			"constant": false,
+			"inputs": [],
+			"name": "unFreeze",
+			"outputs": [
+				{
+					"name": "success",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [],
+			"name": "withdraw",
+			"outputs": [
+				{
+					"name": "result",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "_totalSupply",
+			"outputs": [
+				{
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "_holder",
+					"type": "address"
+				},
+				{
+					"name": "_spender",
+					"type": "address"
+				}
+			],
+			"name": "allowance",
+			"outputs": [
+				{
+					"name": "remaining",
+					"type": "uint256"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "tokenOwner",
+					"type": "address"
+				}
+			],
+			"name": "balanceOf",
+			"outputs": [
+				{
+					"name": "balance",
+					"type": "uint256"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "currentOwner",
+			"outputs": [
+				{
+					"name": "",
+					"type": "address"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "currentRate",
+			"outputs": [
+				{
+					"name": "",
+					"type": "uint64"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "currentStatus",
+			"outputs": [
+				{
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "decimals",
+			"outputs": [
+				{
+					"name": "",
+					"type": "uint8"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "isSoldout",
+			"outputs": [
+				{
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "name",
+			"outputs": [
+				{
+					"name": "",
+					"type": "string"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "owner",
+			"outputs": [
+				{
+					"name": "",
+					"type": "address"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "rate",
+			"outputs": [
+				{
+					"name": "",
+					"type": "uint64"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "symbol",
+			"outputs": [
+				{
+					"name": "",
+					"type": "string"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "totalSupply",
+			"outputs": [
+				{
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		}
+	];
+	
+	
+	const goldmint = web3.eth.contract(contractABI).at(contractAddress);
+
+	function currentRate(){
+		var currentRate;
+		goldmint.currentRate((err,res)=>{
+				if(res) {
+					currentRate = res;
+					$("#currentRate").text("1 ETH : "+ res +" GMT");
+				}
+				else{
+					$("#currentRate").text("Not available");
+				}
+			})  
+		}
+
+	function setRate(){
+		if($("#newRate").val()!=currentRate){
+			console.log($("#newRate").val());
+			goldmint.setRate($("#newRate").val(),(err,res)=>{
+				if(res){
+					$("#btnRate").attr('disabled',true);
+					$("#info1").show();
+				}
+			});
+		}
+	}
+
+	function transfer() {
+		console.log(web3.toWei($("#gmtAmount").val()));
+		goldmint.transfer($("#address").val(),web3.toWei($("#gmtAmount").val(),"ether"),(err,res)=>{
+			if(res) {
+				$("#info3").show();
+				$("#gmtAmount").val("");
+				$("#address").val("");
+			}
+			else	alert("Something went wrong! Please try later");
+		});
+	}
+
+	function freeze(){
+		goldmint.freeze((err,res)=>{
+			if(res){
+				$('#freeze').attr('disabled',true);
+				$("#unFreeze").attr('disabled',true);
+			}
+		});
+	}
+
+	function unFreeze(){
+		goldmint.unFreeze((err,res)=>{
+			if(res){
+				$('#freeze').attr('disabled',true);
+				$("#unFreeze").attr('disabled',true);
+			}
+		});
+	}
+
+	function changeOwner(){
+		goldmint.transferOwnership($("#OwnerAddress").val(),(err,res)=>{
+			if(res){
+				$("#info4").show();
+				$("#BtnChangeOwner").attr('disabled',true);
+			}
+		});
+	}
+
+	function withdraw(){
+		goldmint.withdraw((err,res)=>{
+			if(res){
+				$("#info2").show();
+				$("#btnWithdraw").attr('disabled',true);
+			}
+			else	alert("Something went wrong. Please try again");
+		})
+	}
+
+	function currentStatus(){
+		goldmint.currentStatus((err,res)=>{
+			if(!err){
+				if(res){
+					$("#unFreeze").attr('disabled',false);
+					$("#freeze").attr('disabled',true);
+					$("#description").text("The function allows the owner to unfreeze the contracts in case if it is freezed");
+				}
+				else
+				{
+					$("#freeze").attr('disabled',false);
+					$("#unFreeze").attr('disabled',true);
+					$("#description").html("The function allows the owner to freeze/pause the contract for a while.<br> This will not initiate any transaction via contract during this period");
+				}
+			}
+		});
+	}
+
+	
+	var transferEvent = goldmint.Transfer();
+
+	transferEvent.watch(function(error, result){
+		if (!error){
+			$("#info3").hide();
+		}
+	});
+
+	var rateChangeEvent = goldmint.SetRate();
+
+	rateChangeEvent.watch(function(error, result){
+	if (!error)
+		{		  
+			currentRate();
+			$("#info1").hide();
+			$("#btnRate").attr('disabled',false);
+			$("#newRate").val("");
+		}
+	});
+
+	var ownerChangeEvent = goldmint.OwnershipTransferred();
+	ownerChangeEvent.watch(function(err,res){
+		if(!err){
+			$("#info4").hide();
+			$("#BtnChangeOwner").attr('disabled',false);
+			$("#OwnerAddress").val("");
+		}
+	});
+
+	var statusChangeEvent = goldmint.Freeze();
+	statusChangeEvent.watch(function(err,res){
+		if(!err){
+			currentStatus();
+		}
+	});
+
+	
